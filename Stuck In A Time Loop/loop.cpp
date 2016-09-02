fe92df1a9e6c8123196d916c8efc7276a5b59f22
Dalton Cole <dalton_cl@yahoo.com>
2016-09-02 15:34:55 -0500

    Stuck in a time loop finished

---
+++ Stuck In A Time Loop/loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int runs = 0;
+
+	cin >> runs;
+
+	for(int i = 0; i < runs; i++) {
+		cout << i + 1 << " Abracadabra" << endl;
+	}
+
+	return 0;
+}
